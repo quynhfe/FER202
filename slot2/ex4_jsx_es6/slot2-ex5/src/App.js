808551af8855eba07e20be3f682dc4aac3ba2f71
Quynh <quynhfe99@gmail.com>
2025-08-11 10:42:04 +0700

    add slot 2

---
+++ slot2/ex4_jsx_es6/slot2-ex5/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HelloReactApp from './exercise/Ex1';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloReactApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ slot2/ex4_jsx_es6/slot2-ex5/src/exercise/Ex1.jsx
@@ -0,0 +1,6 @@
+import img from '../asset/img/1.png';
+export default function Ex1() {
+  return (
+    <img src={img} alt="Hình ảnh mô tả" />
+  );
+}
