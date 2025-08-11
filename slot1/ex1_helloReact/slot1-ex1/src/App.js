441c6d95fb78708fdaaad84e27523deb6921ef68
Quynh <quynhfe99@gmail.com>
2025-08-11 11:30:41 +0700

    done

---
+++ slot1/ex1_helloReact/slot1-ex1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>My name is Quynh</p>
+    </div>
+  );
+}
+
+export default App;
