ffc8a7f96831db1c59e1035d35982287f6ec7a31
Quynh <quynhfe99@gmail.com>
2025-08-11 11:10:57 +0700

    fix show people from  list to table

--- slot2/ex6_listPeople/ex6/src/PeopleList.jsx
+++ slot2/ex6_listPeople/ex6/src/PeopleList.jsx
@@ -1,22 +1,37 @@
-const People = [
-  { name: 'Jack', age: 50 },
-  { name: 'Michael', age: 9 },
-  { name: 'John', age: 40 },
-  { name: 'Ann', age: 19 },
-  { name: 'Elisabeth', age: 16 }
-]
+import React from 'react';
+import { Table } from 'antd';
 
-function PeopleList() {
+const people = [
+  { key: 1, name: 'Jack', age: 50 },
+  { key: 2, name: 'Michael', age: 9 },
+  { key: 3, name: 'John', age: 40 },
+  { key: 4, name: 'Ann', age: 19 },
+  { key: 5, name: 'Elisabeth', age: 16 }
+];
+
+const columns = [
+  {
+    title: 'Name',      
+    dataIndex: 'name',   
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  }
+];
+
+export default function PeopleTable() {
   return (
-    <ul>
-      <p>People List</p>
-      {People.map((person, index) => (
-        <li key={index}>
-          {person.name} - {person.age} years old
-        </li>
-      ))}
-    </ul>
-  )
+    <div className='App'>
+      <h1>People List</h1>
+      <Table
+      dataSource={people}  
+      columns={columns}     
+      pagination={false}    
+      bordered              
+    />
+    </div>
+  );
 }
-
-export default PeopleList
