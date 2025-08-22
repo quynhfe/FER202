57aa06b8d9bb90ea29303c12254ebccce47b4377
Quynh <quynhfe99@gmail.com>
2025-08-22 09:33:40 +0700

    update

--- slot17/ex2-usecontext/src/App.js
+++ slot17/ex2-usecontext/src/App.js
@@ -4,37 +4,7 @@ import DishesList from "./components/DishesList";
 import Cart from "./components/Cart";
 import "./styles/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const dishes = [
-  {
-    id: 0,
-    name: "Uthappizza",
-    image: "images/uthappizza.jpeg",
-    price: "4.99",
-    description: "A unique combination of Indian Uthappam and Italian pizza.",
-  },
-  {
-    id: 1,
-    name: "Zucchipakoda",
-    image: "images/zucchipakoda.jpeg",
-    price: "1.99",
-    description: "Deep fried Zucchini with chickpea batter.",
-  },
-  {
-    id: 2,
-    name: "Vadonut",
-    image: "images/vadonut.jpeg",
-    price: "1.99",
-    description: "A combination of vada and donut.",
-  },
-  {
-    id: 3,
-    name: "ElaiCheese Cake",
-    image: "images/elaicheesecake.jpeg",
-    price: "2.99",
-    description: "New York Style Cheesecake with Indian cardamoms.",
-  },
-];
+import { dishes } from "./data/data";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
---
+++ slot17/ex2-usecontext/src/data/data.js
@@ -0,0 +1,30 @@
+export const dishes = [
+  {
+    id: 0,
+    name: "Uthappizza",
+    image: "images/uthappizza.jpeg",
+    price: "4.99",
+    description: "A unique combination of Indian Uthappam and Italian pizza.",
+  },
+  {
+    id: 1,
+    name: "Zucchipakoda",
+    image: "images/zucchipakoda.jpeg",
+    price: "1.99",
+    description: "Deep fried Zucchini with chickpea batter.",
+  },
+  {
+    id: 2,
+    name: "Vadonut",
+    image: "images/vadonut.jpeg",
+    price: "1.99",
+    description: "A combination of vada and donut.",
+  },
+  {
+    id: 3,
+    name: "ElaiCheese Cake",
+    image: "images/elaicheesecake.jpeg",
+    price: "2.99",
+    description: "New York Style Cheesecake with Indian cardamoms.",
+  },
+];
